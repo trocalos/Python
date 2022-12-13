74b8b96f778522bc315c7957f086b379ef01e426
Jose Luis Garcia Vazquez <jgarci32@xtec.cat>
2022-12-13 08:34:22 +0100

    Add files via upload

---
+++ while2.py
@@ -0,0 +1,7 @@
+print('Escribe números de una sola cifra')
+print('Más de una cifra, será equivalente a terminar')
+numero=0
+while numero<10:
+    numero=input('Escribe un número: ')
+    numero=eval(numero)
+    print("lolo")
